3d7f354e47ffe7696db1ad10c02e7a9e74ec6825
Jeff Schenck <jmschenck@gmail.com>
2012-08-17 19:48:24 -0700

    Use differing charge amounts in live test suite to avoid duplicate charge errors

--- tests/test_live.py
+++ tests/test_live.py
@@ -4,6 +4,7 @@ Tests against the sandbox Authorize.net API. Slow, requires internet.
 
 from datetime import date, timedelta
 import os
+import random
 
 from unittest2 import skipUnless, TestCase
 
@@ -24,6 +25,11 @@ TEST_TRANSACTION_KEY = '58JKJ4T95uee75wd'
 @skipUnless(os.environ.get('AUTHORIZE_LIVE_TESTS'), SKIP_MESSAGE)
 class AuthorizeLiveTests(TestCase):
     def setUp(self):
+        # Random in testing feels gross, otherwise running the same test
+        # suite in quick succession produces failures because Authorize.net
+        # thinks the transactions are duplicates and rejects them
+        self.amount1 = random.randrange(100, 100000) / 100.0
+        self.amount2 = random.randrange(100, 100000) / 100.0
         self.client = AuthorizeClient(TEST_LOGIN_ID, TEST_TRANSACTION_KEY)
         self.year = date.today().year + 10
         self.credit_card = CreditCard('4111111111111111', self.year, 1, '911',
@@ -32,23 +38,23 @@ class AuthorizeLiveTests(TestCase):
 
     def test_credit_card(self):
         card = self.client.card(self.credit_card, self.address)
-        transaction = card.auth(10)
+        transaction = card.auth(self.amount1)
         transaction.void()
         self.assertRaises(AuthorizeResponseError, transaction.settle)
 
     def test_saved_card(self):
         card = self.client.card(self.credit_card, self.address)
         saved = card.save()
-        saved.auth(10).settle()
-        saved.capture(10)
-        saved.credit(10)
+        saved.auth(self.amount1).settle()
+        saved.capture(self.amount1)
+        saved.credit(self.amount1)
         saved_from_id = self.client.saved_card(saved.uid)
         saved_from_id.delete()
 
     def test_recurring(self):
         card = self.client.card(self.credit_card, self.address)
         start = date.today() + timedelta(days=7)
-        recurring = card.recurring(10, start, months=1, occurrences=10)
-        recurring.update(amount=20, trial_amount=10, trial_occurrences=3)
+        recurring = card.recurring(self.amount1, start, months=1, occurrences=10)
+        recurring.update(amount=self.amount2, trial_amount=self.amount2 - 0.5, trial_occurrences=3)
         recurring_from_id = self.client.recurring(recurring.uid)
         recurring_from_id.delete()
